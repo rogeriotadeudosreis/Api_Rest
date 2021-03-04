988fb85bb2410a522ee9692c4a517dc91e871acf
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-03-04 11:58:03 -0300

    adicionada uma linha de comentário

--- src/main/java/com/products/apirest/entity/Product.java
+++ src/main/java/com/products/apirest/entity/Product.java
@@ -15,8 +15,8 @@ public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // id geraldo automaticamente incrementado
 	private Long id;
 
 	private String nome;
